8917d46351422729314432e08620b7ba91d52493
MD. Alif Khan <office.alifkhan@gmail.com>
2026-06-15 15:15:57 +0600

    update SafeAreaView components to include edges for improved layout

--- app/(tab)/home.tsx
+++ app/(tab)/home.tsx
@@ -87,7 +87,7 @@ export default function Home() {
   }));
 
   return (
-    <SafeAreaView className="flex-1 bg-slate-50">
+    <SafeAreaView edges={["top", "left", "right"]} className="flex-1 bg-slate-50">
       <ScrollView
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 120 }}
